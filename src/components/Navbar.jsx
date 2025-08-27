113536cce1d80db03db860e3235d8ae7b510aab6
Riya Shrestha <ryeashrestha@gmail.com>
2025-08-27 16:54:58 +0545

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className="header">
         <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-        <p className="blue-gradient_text">3D</p>
+        <p className="blue-gradient_text">RS</p>
         </NavLink>
 
         <nav className="flex text-lg gap-7 font-medium">
